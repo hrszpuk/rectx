d0f8cb89ea21478e0205a7f5d3a7cb2ef353b0fe
Remy <107559570+hrszpuk@users.noreply.github.com>
2022-09-07 20:53:36 +0100

    Began working on config loading
    Other than some issues with error handling that needs to be improved, config file loading should be done soon.
    Currently, the code is untested, so expect major refactors later on.

--- src/main.rs
+++ src/main.rs
@@ -153,3 +153,42 @@ mod manager {
         Ok(())
     }
 }
+
+
+mod config {
+    use std::fs;
+    use std::process::exit;
+    use serde_derive::{Deserialize, Serialize};
+
+
+    #[derive(Debug, Deserialize, Serialize)]
+    struct Config {
+        pub project_name: String,
+    }
+
+    impl Config {
+        pub fn new(name: String) -> Config {
+            Config {
+                project_name: name,
+            }
+        }
+
+        pub fn generate(&self, path: String) -> std::io::Result<()> {
+            let toml_data = match toml::to_string(&self) {
+                Ok(data) => data,
+                Err(error) => {  // Temporary hacky fix -- definitely change later
+                    println!("ERROR! {:?}", error);
+                    exit(1);
+                    String::new()
+                }
+            };
+            fs::write(path + "/config.toml", toml_data)?;
+            Ok(())
+        }
+
+        pub fn load(path: String) -> std::io::Result<Config> {
+            let data = fs::read(path)?;
+            toml::from_slice(&*data)?
+        }
+    }
+}
