699859fcc75a43307c829f4fa72d90dc11312edc
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-01-12 23:13:40 +0000

    Beginning work on CLI stuff

--- main.go
+++ main.go
@@ -1,42 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
 	"os"
-	"rectx/config"
+	"rectx/utilities"
+)
+
+var (
+	newCmd      = flag.NewFlagSet("new", flag.ExitOnError)
+	buildCmd    = flag.NewFlagSet("build", flag.ExitOnError)
+	runCmd      = flag.NewFlagSet("run", flag.ExitOnError)
+	templateCmd = flag.NewFlagSet("template", flag.ExitOnError)
+	configCmd   = flag.NewFlagSet("config", flag.ExitOnError)
+	projectCmd  = flag.NewFlagSet("project", flag.ExitOnError)
 )
 
 func main() {
-	arguments := os.Args
-	arguments = arguments[1:]
-	if len(arguments) < 1 {
-		fmt.Println("Usage: rectx <command> [arguments]")
-		return
-	} else if arguments[0] == "new" {
-		fmt.Println("Creating a new project")
-		var name string
-		var authors string
+	switch os.Args[1] {
+	case "new":
+		utilities.Check(newCmd.Parse(os.Args[2:]))
+		// project_manager.new
+		// -n --name "project name"
+		// -a --author "author"
+		// -t --template "default"
+		// -d --directory "create project path"
+		// -l --license "obvious"
+		// -v --version "0.1.0"
+		// -vs --version-system "major.minor.patch"
+		// -np --no-prompt
+	case "build":
+		utilities.Check(buildCmd.Parse(os.Args[2:]))
+		// project_manager.build
+		// -p --profile "custom build profile"
+	case "run":
+		utilities.Check(runCmd.Parse(os.Args[2:]))
+		// project_manager.run
+		// -p --profile "custom run profile"
+	case "template":
+		utilities.Check(templateCmd.Parse(os.Args[2:]))
+		// templates. ...
+		// list
+		// add "path/to/file" "name"
+		// snapshot "path/to/folder" "name"
+		// setDefault "name of template"
+		// rename "name" "newname"
+	case "config":
+		utilities.Check(configCmd.Parse(os.Args[2:]))
+		// config. ...
+		// validate
+		// -cf --config-file
+		// -t --templates
+		// -l --licenses
+		// regenerate
+		// -cf --config-file
+		// -t --templates
+		// -l --licenses
+		// reset "config.setting" "value"
+		// --all
+		// set "config.setting" "value"
 
-		fmt.Print("Project name: ")
-		fmt.Scanln(&name)
-
-		fmt.Print("Project authors (space separated): ")
-		fmt.Scanln(&authors)
-
-		fmt.Println("Creating project directory")
-		os.Mkdir(name, os.ModePerm)
-		os.Mkdir(fmt.Sprintf("%s/Source", name), os.ModePerm)
-		os.Mkdir(fmt.Sprintf("%s/Build", name), os.ModePerm)
-		os.Mkdir(fmt.Sprintf("%s/Resources", name), os.ModePerm)
-		f, _ := os.Create(fmt.Sprintf("%s/README.md", name))
-		f.Close()
-		f, _ = os.Create(fmt.Sprintf("%s/Source/main.rct", name))
-		f.Close()
-		f, _ = os.Create(fmt.Sprintf("%s/.gitignore", name))
-		f.Close()
-		f, _ = os.Create(fmt.Sprintf("%s/project.rectx", name))
-		f.Close()
-	} else if arguments[0] == "validate" {
-		config.ValidateConfigFile()
+	case "project":
+		utilities.Check(projectCmd.Parse(os.Args[2:]))
+	default:
+		log.Fatalf("Unknown subcommand \"%s\"!\n", os.Args[1])
 	}
 }
