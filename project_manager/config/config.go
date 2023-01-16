4d88f89ab566f1e53cbd33a2fb2d640240cf967a
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-01-16 10:52:51 +0000

    project_manager.config.CreateDefaultConfig finished

--- project_manager/config/config.go
+++ project_manager/config/config.go
@@ -15,7 +15,7 @@ type (
 
 	BuildConfig struct {
 		Compiler        string
-		Optimisation    string
+		Optimisation    int
 		SourceDirectory string
 		BuildDirectory  string
 		Commands        []string
@@ -23,9 +23,41 @@ type (
 	}
 
 	RunConfig struct {
-		Compiler       string
-		Optimisation   string
-		ExecutablePath string
-		Commands       []string
+		ChangesCauseRebuild bool
+		AlwaysRebuild       bool
+		RebuildOptimisation int
+		Commands            []string
 	}
 )
+
+func CreateDefaultConfig() *Config {
+	runConfig := RunConfig{
+		ChangesCauseRebuild: true,
+		AlwaysRebuild:       false,
+		RebuildOptimisation: 3,
+		Commands:            []string{},
+	}
+
+	buildConfig := BuildConfig{
+		Compiler:        "rgoc",
+		Optimisation:    3,
+		SourceDirectory: "",
+		BuildDirectory:  "",
+		Commands:        []string{},
+		ExecutableName:  "",
+	}
+
+	projectConfig := ProjectConfig{
+		Name:    "",
+		Authors: nil,
+		Version: "",
+	}
+
+	config := Config{
+		Project:      projectConfig,
+		BuildProfile: buildConfig,
+		RunProfile:   runConfig,
+	}
+
+	return &config
+}
