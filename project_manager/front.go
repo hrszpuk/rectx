4c23b373a611efe56284f840d55595ac3237e147
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-01-16 11:35:39 +0000

    Added new prompt

--- project_manager/front.go
+++ project_manager/front.go
@@ -1,7 +1,29 @@
 package project_manager
 
-func new() {
+import (
+	"fmt"
+	projectConfig "rectx/project_manager/config"
+	"rectx/templates"
+	"strings"
+)
 
+func New() {
+	pc := projectConfig.CreateDefaultConfig()
+
+	fmt.Println("Project name: ")
+	fmt.Scanln(&pc.Project.Name)
+
+	var authors string
+	fmt.Println("Authors (comma separated): ")
+	fmt.Scanln(&authors)
+	pc.Project.Authors = strings.Split(authors, ",")
+
+	fmt.Println("Version (MAJOR.MINOR.PATCH): ")
+	fmt.Scanln(&pc.Project.Version)
+
+	templateName := GetTemplate()
+
+	CreateNewProject(pc, templateName)
 }
 
 func build() {
