725506d73c2674397cd650eab61f372551814e3b
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-01-17 08:29:55 +0000

    Added BadStatements for errors

--- templates/statement.go
+++ templates/statement.go
@@ -1,5 +1,28 @@
 package templates
 
+import "fmt"
+
 type Statement interface {
 	Generate()
+}
+
+type BadStatement struct {
+	expectedToken *Token
+	tokenFound    *Token
+}
+
+func NewBadStatement(expected, found *Token) *BadStatement {
+	return &BadStatement{
+		expectedToken: expected,
+		tokenFound:    found,
+	}
+}
+
+func (bs *BadStatement) Generate() {
+	fmt.Printf(
+		"ERROR: Expected Token of type \"%s\" but found Token of type \"%s\" with values of \"%s\"!\n",
+		bs.expectedToken.Kind,
+		bs.tokenFound.Kind,
+		bs.tokenFound.Value,
+	)
 }
