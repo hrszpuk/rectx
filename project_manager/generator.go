0240f0b5da88a1f884ec4df1d4a3668081a90d9b
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-01-17 09:34:23 +0000

    Added template parser and generation to project_manager

--- project_manager/generator.go
+++ project_manager/generator.go
@@ -3,7 +3,9 @@ package project_manager
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	projectConfig "rectx/project_manager/config"
+	"rectx/templates"
 	"rectx/utilities"
 )
 
@@ -11,6 +13,17 @@ func CreateNewProject(config *projectConfig.ProjectConfig, templateName string) 
 	fmt.Println("Generating project...")
 
 	utilities.Check(os.Mkdir(config.Project.Name, 0750))
+
+	f, err := os.ReadFile(utilities.GetRectxPath() + "/templates/" + templateName)
+	utilities.Check(err)
+
+	parser := templates.NewTemplateParser(string(f))
+	statements := parser.Parse()
+
+	_ = exec.Command("cd", config.Project.Name)
+	for _, statement := range statements {
+		statement.Generate(config.Project.Name)
+	}
 
 	fmt.Println("Done!")
 }
