563e027809fd858cb0478314c45b6adad0f89b85
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-01-20 22:15:12 +0000

    Added rectx templates add <path>

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"rectx/project_manager"
 	"rectx/templates"
+	"rectx/utilities"
+	"strings"
 )
 
 func main() {
@@ -37,6 +39,26 @@ func main() {
 					fmt.Printf("%d. %s\n", i+1, files)
 				}
 			}
+		} else if len(os.Args) == 4 {
+			switch os.Args[2] {
+			case "add":
+				if !strings.HasSuffix(os.Args[3], ".rectx.template") {
+					fmt.Printf("Whoops \"%s\" isn't a .rectx.template file!\n", os.Args[3])
+					os.Exit(1)
+				} else {
+					contents, err := os.ReadFile(os.Args[3])
+					utilities.Check(err)
+					file, err := os.Create(utilities.GetRectxPath() + "/templates/" + os.Args[3])
+					_, err = file.Write(contents)
+					utilities.Check(err)
+					utilities.Check(file.Close())
+					fmt.Printf("Added \"%s\" successfully!\n", os.Args)
+					fmt.Println("You can do \"rectx templates list\" to make sure ;)")
+				}
+			default:
+				fmt.Printf("Unknown subcommand \"%s\"! Maybe try rectx templates --help for a list of subcommands.", os.Args[3])
+			}
+
 		}
 	case "config":
 		handleParseErrorAndHelpFlag(configCmd, configCmd.Parse(os.Args[2:]), ShowConfigHelpMenu)
