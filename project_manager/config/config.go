edc057914857c0284961c9fdd82d79d99545d7cf
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-01-27 20:10:23 +0000

    Refactored project config

--- project_manager/config/config.go
+++ project_manager/config/config.go
@@ -72,26 +72,24 @@ func CreateDefaultConfig() *ProjectConfig {
 
 func (config *ProjectConfig) Load(path string) {
 	_, err := toml.DecodeFile(path, config)
-	utilities.Check(err)
+	utilities.Check(err, true, "Attempted to load project config but failed.")
 }
 
 func (config *ProjectConfig) Dump(path string) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		_, err = os.Create(path)
-		utilities.Check(err)
+		utilities.Check(err, true, "Attempted to recover non-existent project config but failed to recover.")
 	}
 
 	f, err := os.OpenFile(path, os.O_WRONLY, os.ModeType)
-	utilities.Check(err)
+	utilities.Check(err, true, "Attempted to open file in write only mode but failed for an unknown reason.")
+
+	defer utilities.Check(f.Close(), true, "Attempt to close project config file and failed... Somehow?")
 
 	buffer := new(bytes.Buffer)
 	err = toml.NewEncoder(buffer).Encode(config)
-	utilities.Check(err)
+	utilities.Check(err, true, "Attempted to deserialize config data but failed.")
 
-	n, err := f.WriteString(buffer.String())
-	fmt.Printf("N: %d\n", n)
-	utilities.Check(err)
-
-	err = f.Close()
-	utilities.Check(err)
+	_, err = f.WriteString(buffer.String())
+	utilities.Check(err, true, fmt.Sprintf("Attempted to write data to \"%s\" but failed for an unknown reason!", path))
 }
