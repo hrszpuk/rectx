c71b5129fb3ab1fdce700811dd05e63d76033645
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-02-08 20:15:16 +0000

    Added licenses.GenerateLicense

--- licenses/generator.go
+++ licenses/generator.go
@@ -1,5 +1,23 @@
 package licenses
 
-func GenerateLicense(license string) {
+import (
+	"os"
+	"rectx/config"
+	"rectx/metavariables"
+	"rectx/utilities"
+)
 
+func GenerateLicense(license string, variables map[string]string) {
+	config.ValidateLicenses()
+	bytes, err := os.ReadFile(utilities.GetRectxPath() + "/licenses/" + license)
+	utilities.Check(err, true, "Failed to fetch license file specified!")
+	content := string(bytes)
+	content = metavariables.NewParser(content, variables).Parse()
+
+	file, err := os.Create(variables["%PROJECT_NAME%"] + "/" + "LICENSE")
+	utilities.Check(err, false, "Failed to create LICENSE file for project!")
+	_, err = file.WriteString(content)
+	utilities.Check(err, false, "Attempted to write content to LICENSE file but failed.")
+	err = file.Close()
+	utilities.Check(err, false, "Failed to close file!")
 }
