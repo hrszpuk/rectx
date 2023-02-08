140a8b6078862c17314a33d9004923dbc1ddcae6
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-02-08 22:20:44 +0000

    Licenses are now displayed with spaces instead of underscores

--- licenses/prompt.go
+++ licenses/prompt.go
@@ -1,8 +1,11 @@
 package licenses
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
+	"strings"
 )
 
 // Prompt Handles getting the license from a user.
@@ -10,17 +13,21 @@ import (
 // If "None" is selected then prompt will return "None" (You must handle this output if so).
 func Prompt() string {
 	var selectedLicense string
+	in := bufio.NewReader(os.Stdin)
 
 	for i, license := range LICENSES {
-		fmt.Printf("%d. %s\n", i+1, license)
+		fmt.Printf("%d. %s\n", i+1, strings.ReplaceAll(license, "_", " "))
 	}
 	fmt.Print("Chosen license: ")
-	fmt.Scanln(&selectedLicense)
+
+	selectedLicense, _ = in.ReadString('\n')
+	selectedLicense = selectedLicense[0 : len(selectedLicense)-1]
 
 	if index, err := strconv.Atoi(selectedLicense); err == nil {
 		selectedLicense = LICENSES[index-1]
 		return selectedLicense
 	} else {
+		selectedLicense = strings.ReplaceAll(selectedLicense, " ", "_")
 		for _, license := range LICENSES {
 			if selectedLicense == license {
 				return selectedLicense
